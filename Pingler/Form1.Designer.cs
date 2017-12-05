a2b56fd8c70503f8e3e0d35caac0cede447ed2b5
Yavuz Selim <yavuzselimuzun@gmail.com>
2017-12-05 15:13:29 +0300

    Pingler is working.

---
+++ Pingler/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Pingler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pingbtn = new System.Windows.Forms.Button();
+            this.rprbtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.islemlabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 277);
+            this.listBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(248, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(239, 277);
+            this.listBox2.TabIndex = 2;
+            // 
+            // pingbtn
+            // 
+            this.pingbtn.BackColor = System.Drawing.Color.White;
+            this.pingbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.pingbtn.Image = global::Pingler.Properties.Resources.play_button;
+            this.pingbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.pingbtn.Location = new System.Drawing.Point(167, 295);
+            this.pingbtn.Name = "pingbtn";
+            this.pingbtn.Size = new System.Drawing.Size(168, 62);
+            this.pingbtn.TabIndex = 4;
+            this.pingbtn.Text = "Ping to all domain names";
+            this.pingbtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.pingbtn.UseVisualStyleBackColor = false;
+            this.pingbtn.Click += new System.EventHandler(this.pingbtn_Click);
+            // 
+            // rprbtn
+            // 
+            this.rprbtn.BackColor = System.Drawing.Color.White;
+            this.rprbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rprbtn.Image = global::Pingler.Properties.Resources.search;
+            this.rprbtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.rprbtn.Location = new System.Drawing.Point(341, 295);
+            this.rprbtn.Name = "rprbtn";
+            this.rprbtn.Size = new System.Drawing.Size(147, 62);
+            this.rprbtn.TabIndex = 3;
+            this.rprbtn.Text = "Save as report";
+            this.rprbtn.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.rprbtn.UseVisualStyleBackColor = false;
+            this.rprbtn.Click += new System.EventHandler(this.rprbtn_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = global::Pingler.Properties.Resources.plus;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(12, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 62);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Domain Names";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.islemlabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 394);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(500, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // islemlabel
+            // 
+            this.islemlabel.ForeColor = System.Drawing.Color.Black;
+            this.islemlabel.Name = "islemlabel";
+            this.islemlabel.Size = new System.Drawing.Size(12, 17);
+            this.islemlabel.Text = "-";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 363);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(475, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(500, 416);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.pingbtn);
+            this.Controls.Add(this.rprbtn);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pingler";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button rprbtn;
+        private System.Windows.Forms.Button pingbtn;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel islemlabel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Pingler/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace Pingler
+{
+    
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+
+        // Button of add domain names list.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            selectdomain = 0;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            progressBar1.Value = 0;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader oku = new StreamReader(openFileDialog1.FileName);
+                string satir = oku.ReadLine();
+
+
+                //Warning if text file is empty.
+                if (satir == null)
+                {
+                    MessageBox.Show("You can not select an empty file.", "Selected File Empty", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                while (satir != null)
+                {
+                    pingbtn.Enabled = true;
+                    listBox1.Items.Add(satir);
+                    satir = oku.ReadLine();
+                }
+
+            }
+
+        }
+
+        private void pingbtn_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        //Selected domain name value.
+        int selectdomain;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            pingbtn.Enabled = false;
+            int alldomainsnumber = listBox1.Items.Count;
+            progressBar1.Maximum = alldomainsnumber;
+            listBox1.SelectedIndex = selectdomain;
+            islemlabel.Text = "In the process: " + Convert.ToString(listBox1.SelectedItem);
+            progressBar1.Value += 1;
+
+            try
+            {
+                Ping pingdomain = new Ping();
+                PingReply replydomain = pingdomain.Send(Convert.ToString(listBox1.SelectedItem), 1000);
+                if (replydomain != null)
+                {
+                    listBox2.Items.Add(Convert.ToString(listBox1.SelectedItem) + " | " + replydomain.Address);
+                }
+                
+            }
+
+            catch
+            {
+                listBox2.Items.Add(Convert.ToString(listBox1.SelectedItem) + " | Error!");
+            }
+
+            selectdomain++;
+            if (selectdomain == alldomainsnumber)
+            {
+                timer1.Stop();
+                islemlabel.Text = "In the process: " + Convert.ToString(listBox1.SelectedItem);
+                timer2.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pingbtn.Enabled = false;
+            rprbtn.Enabled = false;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            islemlabel.Text = "The process is complete.";
+            timer2.Stop();
+            listBox1.Items.Clear();
+            pingbtn.Enabled = false;
+            rprbtn.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private void rprbtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Coming Soon :] ");
+        }
+    }
+}
